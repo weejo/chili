8aa0609c5f1a6912993e3b6e8939c146b97c1e1e
weejo <91460362+weejo@users.noreply.github.com>
2022-03-15 13:41:18 +0100

    finished logging

---
+++ ChiliEngine/src/Chili/Log.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Core.h"
+#include "spdlog\spdlog.h"
+
+
+namespace Chili {
+
+	class CHILI_API Log
+	{
+	public: 
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+
+	};
+}
+
+#define CH_CORE_TRACE(...) :::Chili::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CH_CORE_INFO(...) :::Chili::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CH_CORE_WARN(...) :::Chili::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CH_CORE_ERROR(...) :::Chili::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CH_CORE_FATAL(...) :::Chili::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+#define CH_TRACE(...) :::Chili::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CH_INFO(...) :::Chili::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CH_WARN(...) :::Chili::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CH_ERROR(...) :::Chili::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CH_FATAL(...) :::Chili::Log::GetClientLogger()->fatal(__VA_ARGS__)
--- ChiliEngine/src/chili.h
+++ ChiliEngine/src/chili.h
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include "Chili\Application.h"
+#include "Chili\Log.h"
 
 // --- ENTRY POINT ----
 #include "Chili\EntryPoint.h"
