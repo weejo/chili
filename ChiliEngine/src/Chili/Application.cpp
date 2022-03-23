e9f9c1e8b965cf817b4c8b210550e5d130b5259b
weejo <91460362+weejo@users.noreply.github.com>
2022-03-23 11:47:23 +0100

    GLFW

--- ChiliEngine/src/Chili/Application.cpp
+++ ChiliEngine/src/Chili/Application.cpp
@@ -8,14 +8,28 @@
 #include <GLFW\glfw3.h>
 
 namespace Chili {
+
+// makro to bind event functions
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
+
+	void Application::OnEvent(Event& e) {
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		CH_CORE_INFO("{0}", e);
+	}
+
 
 	void Application::Run()
 	{
@@ -24,6 +38,12 @@ namespace Chili {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+	
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
 
--- ChiliEngine/src/Chili/Application.h
+++ ChiliEngine/src/Chili/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events\Event.h"
+#include "Chili\Events\ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Chili {
@@ -12,7 +14,11 @@ namespace Chili {
 		virtual ~Application();
 
 		void Run();
+		
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 		
